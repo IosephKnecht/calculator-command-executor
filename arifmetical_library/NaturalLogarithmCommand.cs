73eb61de99196382fcbb9ded23e1093a66d7484d
Dalexp <wertyu183@yandex.ru>
2018-11-04 21:05:31 +0300

    add_NaturalLogarithmCommand
    добавлено натуральное логарифмирование

---
+++ arifmetical_library/NaturalLogarithmCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using app;
+
+namespace arifmetical_library
+{
+    public class NaturalLogarithmCommand:OneOperandCommand
+    {
+        public override double Execute()
+        {
+            return Math.Log(operand);
+        }
+    }
+}
