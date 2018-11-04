89e1f20ebc9ddd530f8059fbac6714856ade012e
Dalexp <wertyu183@yandex.ru>
2018-11-04 17:08:39 +0300

    add_DivisionWithRemainderCommand
    добавлена операция деления с остатком

---
+++ arifmetical_library/DivisionWithRemainderCommand.cs
@@ -0,0 +1,12 @@
+﻿using app;
+
+namespace arifmetical_library
+{
+    public class DivisionWithRemainderCommandl:TwoOperandCommand
+    {
+        public override double Execute()
+        {
+            return firstOperand % secondOperand;
+        }
+    }
+}
