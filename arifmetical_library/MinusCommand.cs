69764dc2646481e7063dc91fda479a6bae36b582
Dalexp <wertyu183@yandex.ru>
2018-11-04 15:34:49 +0300

    add_MinusCommand
    Добавлена арифметическая операция вычитание

---
+++ arifmetical_library/MinusCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using app;
+
+namespace arifmetical_library
+{
+    public class MinusCommand:TwoOperandCommand
+    {
+        public override double Execute()
+        {
+            return firstOperand - secondOperand;
+        }
+    }
+}
