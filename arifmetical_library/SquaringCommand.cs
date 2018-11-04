3a5c84d5d98f48409f888c969c3107cdd82e0ca9
Dalexp <wertyu183@yandex.ru>
2018-11-04 18:58:42 +0300

    add_SquaringCommand
    добавлена команда возведения в квадрат

---
+++ arifmetical_library/SquaringCommand.cs
@@ -0,0 +1,12 @@
+﻿using app;
+
+namespace arifmetical_library
+{
+    public class SquaringCommand:OneOperandCommand
+    {
+        public override double Execute()
+        {
+            return operand * operand;
+        }
+    }
+}
