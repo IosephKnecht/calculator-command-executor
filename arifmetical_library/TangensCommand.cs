b86ba75596fc58ab3e5cbc0bac2761f12a8bb9cf
Dalexp <wertyu183@yandex.ru>
2018-11-04 20:16:12 +0300

    add_TangensCommand
    добавлена тригонометрическая функция тангенс

---
+++ arifmetical_library/TangensCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using app;
+
+namespace arifmetical_library
+{
+    public class TangensCommand:OneOperandCommand
+    {
+        public override double Execute()
+        {
+            return Math.Cos(operand);
+        }
+    }
+}
