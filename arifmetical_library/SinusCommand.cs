cd1df6a3b52d6657a0d1c52e7e33e2f2e1d8ffce
Dalexp <wertyu183@yandex.ru>
2018-11-04 19:45:48 +0300

    add_SinusCommand
    добавлена тригонометрическая функция синус

---
+++ arifmetical_library/SinusCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using app;
+
+namespace arifmetical_library
+{
+    public class SinusCommand:OneOperandCommand
+    {
+        public override double Execute()
+        {
+            return Math.Sin(operand);
+        }
+    }
+}
