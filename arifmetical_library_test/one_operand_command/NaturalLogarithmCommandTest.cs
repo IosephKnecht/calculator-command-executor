ff6739adada57447e1085cb37b65d209f82b4760
Dalexp <wertyu183@yandex.ru>
2018-11-06 19:41:49 +0300

    add_NaturalLogarithmCommandTest
    добавлен тест натурального логарифмирования

---
+++ arifmetical_library_test/one_operand_command/NaturalLogarithmCommandTest.cs
@@ -0,0 +1,18 @@
+﻿using arifmetical_library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace arifmetical_library_test.one_operand_command
+{
+    [TestClass]
+    public class NaturalLogarithmCommandTest : AbstractOneOperandCommandTest
+    {
+        [TestMethod]
+        public override void TestOneOperandCommand()
+        {
+            setCommand(new NaturalLogarithmCommand());
+            SetOperand(1);
+            SetExpectedValue(0);
+            base.TestOneOperandCommand();
+        }
+    }
+}
