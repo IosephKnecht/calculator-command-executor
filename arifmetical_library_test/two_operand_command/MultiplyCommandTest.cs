85a31ee5d2f1d62bc9dc0833b5768d1c02c91373
Dalexp <wertyu183@yandex.ru>
2018-11-06 19:34:08 +0300

    add_MultiplyCommandTest
    добавлен тест операции умножения

---
+++ arifmetical_library_test/two_operand_command/MultiplyCommandTest.cs
@@ -0,0 +1,19 @@
+﻿using arifmetical_library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace arifmetical_library_test.two_operand_command
+{
+    [TestClass]
+    public class MultiplyCommandTest : AbstractTwoOperandCommandUnitTest
+    {
+        [TestMethod]
+        public override void TestTwoOperandCommand()
+        {
+            SetCommand(new MultiplyCommand());
+            SetFistOperand(35);
+            SetSecondOperand(20);
+            SetExpectedValue(700);
+            base.TestTwoOperandCommand();
+        }
+    }
+}
