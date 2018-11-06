e6cc455972b44708a06d0d86f823b52e225a1060
Dalexp <wertyu183@yandex.ru>
2018-11-06 18:44:52 +0300

    add_ArctangensCommandTest
    добавлен тест арктангенса

---
+++ arifmetical_library_test/one_operand_command/ArctangensCommandTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using arifmetical_library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace arifmetical_library_test.one_operand_command
+{
+    [TestClass]
+    public class ArctangensCommandTest:AbstractOneOperandCommandTest
+    {
+        [TestMethod]
+        public override void TestOneOperandCommand()
+        {
+            setCommand(new ArctangesCommand());
+            SetOperand(0);
+            SetExpectedValue(0);
+            base.TestOneOperandCommand();
+        }
+    }
+}
